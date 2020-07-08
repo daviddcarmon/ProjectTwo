60a4aad7d517a35edcd9bc98f46118c763ee5174
Brittanie Barrett <britt.barrett302@gmail.com>
2020-07-07 19:48:47 -0700

    update to schema file

--- db/schema.sql
+++ db/schema.sql
@@ -4,18 +4,19 @@ CREATE DATABASE duelApp_db;
 
 USE duelApp_db;
 
-
-CREATE TABLE user(
-
-    id int primary key,
-    name varchar(50),
-    username VARCHAR,
-    email varchar(75)
-);
-
 CREATE TABLE characters(
     id int primary key,
     name varchar(50),
     spell varchar(50),
     stats varchar(75)
+);
+
+CREATE TABLE user(
+
+    id int primary key,
+    name varchar(50),
+    username VARCHAR(50),
+    email varchar(75),
+    characterid_fk int,
+    foreign key (characterid_fk) references characters(id)
 );
